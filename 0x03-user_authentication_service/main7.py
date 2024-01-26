38c49714e4aef65996ca3c41b15b7a37c49fc463
realjioke <scarlobest@gmail.com>
2024-01-26 04:58:39 +0000

    main7.py

---
+++ 0x03-user_authentication_service/main7.py
@@ -0,0 +1,12 @@
+in file
+"""
+from auth import Auth
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
